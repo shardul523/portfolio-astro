4014772279f50a3f465066266a1e14b2d043dc17
Shardul Sisodiya <98705625+shardul523@users.noreply.github.com>
2023-10-12 02:40:36 +0530

    Added Intro and About sections

--- src/components/navbar/NavContainer.jsx
+++ src/components/navbar/NavContainer.jsx
@@ -2,7 +2,7 @@ import navbar from "./navbar.module.css";
 
 const NavContainer = ({ children, isMobile }) => (
   <nav
-    className={`${navbar.flex} ${navbar.shadow} ${
+    className={`${navbar.navbar} ${navbar.flex} ${navbar.shadow} ${
       isMobile && navbar.flex_column
     } ${!isMobile && navbar.min_h_15} ${!isMobile && navbar.align_center} ${
       isMobile ? navbar.justify_center : navbar.justify_space_around
